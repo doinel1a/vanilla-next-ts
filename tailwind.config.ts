1aa8f4b14d0cf791c5181c823e7e710c747d56bf
doinel1a <doinel1atanasiu@gmail.com>
2023-08-14 15:52:54 +0200

    refactor: tailwind base styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,17 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        tertiary: 'var(--tertiary)',
+        'color-light': 'var(--color-light)',
+        'color-dark': 'var(--color-dark)',
+        'accent-primary': 'var(--accent-primary)',
+        'accent-primary-state': 'var(--accent-primary-state)'
       }
     }
   },
