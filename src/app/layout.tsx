b921462b1668ab228daec099057e27354806d305
doinel1a <doinel1atanasiu@gmail.com>
2023-08-14 16:04:06 +0200

    feat: postcss + config + sass + font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-import './globals.css';
+import '../styles/globals.css';
+import '../styles/globals.scss';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import React from 'react';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
