eb1c7bb3e4d29a6197706fb71fe61aea1b083ef1
doinel1a <doinel1atanasiu@gmail.com>
2023-08-14 15:24:29 +0200

    fix: eslint plugins conflict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,81 +1,72 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	plugins: [],
-	extends: [
-		'next/core-web-vitals',
-		'plugin:prettier/recommended',
-		'plugin:promise/recommended',
-		'plugin:jsx-a11y/recommended',
-		'plugin:sonarjs/recommended',
-		'plugin:unicorn/recommended',
-		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	settings: {
-		'import/parsers': {
-			'@typescript-eslint/parser': ['.ts', '.tsx'],
-		},
-		'import/resolver': {
-			node: {
-				extensions: ['.ts', '.tsx'],
-				moduleDirectory: ['node_modules', 'src/'],
-			},
-			typescript: {
-				alwaysTryTypes: true,
-			},
-		},
-		react: {
-			pragma: 'React',
-			version: 'detect',
-		},
-	},
-	ignorePatterns: ['**/*.html'],
-	rules: {
-		// base
-		indent: ['error', 2, { SwitchCase: 1 }],
-		'linebreak-style': ['error', 'windows'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		// end
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  plugins: [],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+    'plugin:promise/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/']
+      },
+      typescript: {
+        alwaysTryTypes: true
+      }
+    },
+    react: {
+      pragma: 'React',
+      version: 'detect'
+    }
+  },
+  ignorePatterns: ['**/*.html'],
+  rules: {
+    // base
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    // end
 
-		// prettier
-		'arrow-body-style': 'off',
-		'prefer-arrow-callback': 'off',
-		'prettier/prettier': [
-			'error',
-			{
-				printWidth: 100,
-				tabWidth: 2,
-				useTabs: false,
-				semi: true,
-				singleQuote: true,
-				jsxSingleQuote: true,
-				trailingComma: 'none',
-				bracketSpacing: true,
-				bracketSameLine: false,
-				arrowParens: 'always',
-				endOfLine: 'crlf',
-			},
-		],
-		// end prettier
-
-		// simple-import-sort
-		'simple-import-sort/imports': 'error',
-		'simple-import-sort/exports': 'error',
-		'import/first': 'error',
-		'import/newline-after-import': 'error',
-		'import/no-duplicates': 'error',
-		// end simple-import-sort
-	},
+    // prettier
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        bracketSameLine: false,
+        arrowParens: 'always',
+        endOfLine: 'crlf'
+      }
+    ]
+    // end prettier
+  }
 };
