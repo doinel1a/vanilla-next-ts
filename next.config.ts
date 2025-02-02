1a0a5ccfc02296c4eecdc3eb34944139a0c217a4
doinel1a <doinel1atanasiu@gmail.com>
2025-02-02 12:16:52 +0100

    chore: enable turbopack & use .ts in next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from 'next';
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
   }
-};
+} satisfies NextConfig;
 
 export default nextConfig;
+
