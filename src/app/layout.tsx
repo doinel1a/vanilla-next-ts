192a94c5a2c6eeba92306c3a071489e3fef42207
doinel1a <doinel1atanasiu@gmail.com>
2025-02-02 12:32:30 +0100

    fix: layout dimensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,17 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <div className='grid min-h-[100dvh] grid-rows-[auto_1fr_auto]'>
+          <header className='flex h-10 items-center justify-center border-b'>
+            <h1>Header</h1>
+          </header>
+          {children}
+          <footer className='flex h-8 items-center justify-center border-t'>
+            <p>Footer</p>
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
