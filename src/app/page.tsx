0dec1c0f6b3b2ac1b09c9b86a8ae08e3f691061a
doinel1a <doinel1atanasiu@gmail.com>
2025-05-08 20:09:21 +0200

    fix: eslint warnings & errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className='flex h-full w-full flex-col items-center justify-center'>
       <h1>Hello, World!</h1>
