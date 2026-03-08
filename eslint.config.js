848cfbadd2790b87bbcd441521f4bd034ed97151
doinel1a <doinel1atanasiu@gmail.com>
2026-03-08 18:36:52 +0100

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import eslintConfigNext from 'eslint-config-next';
 import prettierConfig from 'eslint-config-prettier/flat';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
-import sonarjs from 'eslint-plugin-sonarjs';
+import { configs as sonarjs } from 'eslint-plugin-sonarjs';
 import unicorn from 'eslint-plugin-unicorn';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['.next']
   },
@@ -19,7 +20,7 @@ export default tseslint.config(
   ...eslintConfigNext,
   prettierConfig,
   prettierPlugin,
-  sonarjs.configs.recommended,
+  sonarjs.recommended,
   unicorn.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
